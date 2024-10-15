aa478d9c8d7d3b83fd97abcb75160fcd7aaf0df9
shafiemukhre <shafiemukhre@gmail.com>
2024-10-14 18:27:41 -0700

    test

--- src/components/SearchCustomer.js
+++ src/components/SearchCustomer.js
@@ -1,13 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import CustomerList from './CustomerList'
 import List from '../List';
 
 function SearchCustomer() {
-    
+  const [value, setValue] = useState('');
   return (
     <>
     <div className='layout-row align-items-center justify-content-center mt-30'>
-        <input className='large mx-20 w-20' data-testid='search-input' value='Search' placeholder='Enter search term (Eg: Phil)' />
+        <input className='large mx-20 w-20' data-testid='search-input' value={value} onChange={(e) => setValue(e.target.value)} placeholder='Enter search term (Eg: Phil)' />
     </div>
     <CustomerList />
     </>
